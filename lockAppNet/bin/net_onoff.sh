5d6733464e5f3edc680c0d3084ba1ed952be7578
nx111 <gd.zhangdz@gmail.com>
2016-05-26 17:47:25 +0800

    lockAppNet: just disable lockAppNet one hour.

--- lockAppNet/bin/net_onoff.sh
+++ lockAppNet/bin/net_onoff.sh
@@ -4,9 +4,12 @@ netlock=`getprop android.network.lockAppNet`
 [  "$netlock" = "" ]  && netlock="1"
 if [ "$netlock" = "0" ]; then
    setprop android.network.lockAppNet 1
+   setprop android.lockAppNet.offtime ""
    echo "...... enable lockAppNet......"
 else
    setprop android.network.lockAppNet 0
+   setprop android.lockAppNet.offtime `date +%s`
    echo "...... disable lockAppNet......"
 fi
 [ -x /system/bin/lockAppNet ] && /system/bin/lockAppNet
+
