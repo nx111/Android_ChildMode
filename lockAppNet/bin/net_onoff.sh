4c225ad2b5bef5cbb4f23378b0e51f8afadcfa62
nx111 <gd.zhangdz@gmail.com>
2016-07-18 21:39:23 +0800

    lockAppNet: can switch off lockAppNet until to sometime.

--- lockAppNet/bin/net_onoff.sh
+++ lockAppNet/bin/net_onoff.sh
@@ -1,8 +1,19 @@
 #!/system/bin/sh
+hasOffTo=0
+if [ $# -ge 1 ]; then
+   if echo $1 | grep -q "[[:digit:]]\{1,2\}:[[:digit:]]\{1,2\}"; then
+        local hour=$(echo $1 | cut -d: -f1)
+        local minute=$(echo $1 | cut -d: -f2)
+        if [ $hour -le 23 -a $minute -le 59 ]; then
+            setprop android.lockAppNet.offto $1
+            hasOffTo=1
+        fi
+   fi
+fi
 
 netlock=`getprop android.network.lockAppNet`
 [  "$netlock" = "" ]  && netlock="1"
-if [ "$netlock" = "0" ]; then
+if [ "$netlock" = "0" -a $hasOffTo -eq 0 ]; then
    setprop android.network.lockAppNet 1
    setprop android.lockAppNet.offtime ""
    echo "...... enable lockAppNet......"
@@ -11,6 +22,7 @@ else
    setprop android.lockAppNet.offtime `date +%s`
    echo "...... disable lockAppNet......"
 fi
-
-[ -x /system/bin/lockAppNet -a "$(getprop android.lockAppNet.pid)" = "" ] && /system/bin/lockAppNet
+lockAppNetPid=$(getprop android.lockAppNet.pid)
+[ "$lockAppNetPid" != "" -a -d /proc/$lockAppNetPid ] && exit
+[ -x /system/bin/lockAppNet ] && /system/bin/lockAppNet
 
